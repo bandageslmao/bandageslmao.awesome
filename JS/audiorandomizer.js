b99b235e24fdf0814d9cba767754c48606462225
orange <Sanicbuthighlyondrugs@gmail.com>
2025-09-07 20:59:32 -0500

    audiofix fr this time

--- JS/audiorandomizer.js
+++ JS/audiorandomizer.js
@@ -2,12 +2,20 @@ const bgms = ["supersponge","CosmicEternity","MetallicMadness"]
 audio = document.createElement("audio")
 document.body.appendChild(audio)
 audio.controls = false
+activateautoplay = document.createElement("button")
+activateautoplay.hidden = true
+activateautoplay.textContent = "Hey! your autoplay's off! click me to turn it on!"
+activateautoplay.onclick = () => {
+    audio.play()
+    activateautoplay.hidden = true
+}
+document.body.appendChild(activateautoplay)
 
 function randomizer(){
     audio.src = "Audio/" + bgms[Math.floor(Math.random() * bgms.length)] + ".mp3"
     audio.play().catch((err) => {
-        alert("you cant get audio because autoplay lalalala")
-        })
+        activateautoplay.hidden = false
+    })
 }
 randomizer()
 
