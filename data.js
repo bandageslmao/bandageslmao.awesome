1133b441f52462e64265389742f09f1fb69c3752
spongeman 9 <gregslawnmowing40@gmail.com>
2025-11-13 19:39:40 +0000

    mainpage is done!!!

--- data.js
+++ data.js
@@ -1,34 +1,10 @@
 var Points = localStorage.getItem("Points")
-if (localStorage.getItem("Visited") ===! true){
+if (localStorage.getItem("Visited") === true){
     window.location.href = "./firsttime.html"
     localStorage.setItem("Points",0)
 }
 
 function texttest(){
-    const addition = document.createElement('h1')
-    addition.style.textAlign = 'center'
-    var animation = addition.animate(
-        [
-            { transform: 'translateY(500%)'},
-            { transform: 'translateY(-10%)'},
-            { transform: 'translateY(-10%)'},
-            { transform: 'translateY(-10%)'},
-            { transform: 'translateY(-10%)'},
-            { transform: 'translateY(-10%)'},
-            { transform: 'translateY(125%)'}
-        ],
-        {
-            duration: 3000,
-            iterations: 1
-        }
-    )
-    document.body.appendChild(addition)
-    addition.style.fontFamily = ""
-    addition.style.zIndex = '10'
-    addition.textContent = "YTPS: " + Points
-    animation.addEventListener('finish',function(){
-        addition.remove()
-    })
 }
 
 function gainpoints(amnt){
@@ -37,4 +13,16 @@ function gainpoints(amnt){
     pointsound.volume = 1
     pointsound.play()
     localStorage.setItem("Points",parseInt(Points) + amnt)
+}
+
+function deduction(amnt){
+    if (parseInt(Points) > 0){
+        localStorage.setItem("Points",parseInt(Points)- amnt)
+    }else{
+        console.log('debt')
+    }
+}
+
+if (parseInt(Points) < 0){
+    localStorage.setItem("Points",0)
 }
--- javascript/clicksounds.js
+++ javascript/clicksounds.js
@@ -1,6 +1,7 @@
 function click(){
     var snap = new Audio();
     snap.src = "./sfx/snap.wav";
+    snap.volume = 0.5
     snap.play();
 }
 
@@ -10,6 +11,25 @@ function hover(){
     button.play();
 }
 
+function leave(){
+    var button = new Audio();
+    button.src = "./sfx/Kerplunk.wav"
+    button.play()
+}
+
 document.addEventListener('click', function(event) {
     click();
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var links = document.querySelectorAll('a')
+    var buttons = document.querySelectorAll('button')
+    links.forEach(link => {
+        link.addEventListener('mouseover', hover)
+        link.addEventListener('mouseleave', leave)
+    })
+    buttons.forEach(button => {
+        button.addEventListener('mouseover', hover)
+        button.addEventListener('mouseleave', leave)
+    })
+})
