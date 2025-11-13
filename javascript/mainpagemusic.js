15315bfb0f153603e07afff96647d5eedf9528c7
spongeman 9 <gregslawnmowing40@gmail.com>
2025-11-13 20:42:59 +0000

    mute button

--- javascript/mainpagemusic.js
+++ javascript/mainpagemusic.js
@@ -1,3 +1,4 @@
+var muted = false
 var songs = ["buildyourowngame.mp3","crossroadtimes.mp3","happydayinrobloxhq.mp3","mule.mp3","noobalert.mp3","robloxtheme.mp3"]
 var selectedsong = Math.floor(Math.random()* songs.length)
 var song = new Audio
@@ -14,3 +15,13 @@ function autoplayenabler(){
 song.play().catch(Error => {
     autoplayenabler()
 })
+const musicbutton = document.getElementById('mute-unmute')
+musicbutton.onclick = function muteandunmute(){
+    if (muted === false){
+        muted = true
+        song.pause()
+    }else{
+        muted = false
+        song.play()
+    }
+}
